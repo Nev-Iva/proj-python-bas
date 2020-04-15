34914e3d868566a55ceae9c4aee745a76f7fa0f8
Nev-Iva <63519266+Nev-Iva@users.noreply.github.com>
2020-04-15 16:11:02 +0300

    Исправлен файл
    Добавлена проверка на числа, переименованы переменные

--- hw1/hw_1_6.py
+++ hw1/hw_1_6.py
@@ -1,8 +1,15 @@
-a = int(input('Введите результат первого дня\n'))
-b = int(input('Введите желаемый результат\n'))
+while True:
+    res_1 = input('Введите результат первого дня\n')
+    res_n = input('Введите желаемый результат\n')
+    if res_1.isdigit() and res_n.isdigit():
+        res_1 = int(res_1)
+        res_n = int(res_n)
+        break
+    else:
+        print('Ошибка ввода, введите число')
 day = 1
-while a < b:
-    a *= 1.1
+while res_1 < res_n:
+    res_1 *= 1.1
     day += 1
 else:
-    print('Спортсмен достигнет желаемого результата на %d день' %day)
+    print('Спортсмен достигнет желаемого результата на {0} день'.format(day))
