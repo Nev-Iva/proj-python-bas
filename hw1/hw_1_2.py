640dc3a4669c3f021abb597fe2367d8b9e4ad5bf
Nev-Iva <63519266+Nev-Iva@users.noreply.github.com>
2020-04-15 15:35:09 +0300

    Updated hw1_2
    Добавлена проверка на вводе

--- hw1/hw_1_2.py
+++ hw1/hw_1_2.py
@@ -1,4 +1,10 @@
-inp_sec = int(input('введите время в секундах\n'))
+while True:
+    inp_sec = input('введите время в секундах\n')
+    if inp_sec.isdigit():
+        inp_sec = int(inp_sec)
+        break
+    else:
+        print('Ошибка ввода, введите число')
 var_hour = inp_sec // 3600
 var_min = (inp_sec - var_hour * 3600) // 60
 var_sec = inp_sec - var_hour * 3600 - var_min * 60
